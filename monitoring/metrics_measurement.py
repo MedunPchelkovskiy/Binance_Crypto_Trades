5939fd0d7a5a2135c0a8f33489d84a09c58e53ee
Medun <donhuanmatus62@gmail.com>
2026-09-06 22:44:38 +0300

    fix bug, key error(p95_latency_ms) in measure_batch

--- monitoring/metrics_measurement.py
+++ monitoring/metrics_measurement.py
@@ -16,6 +16,6 @@ def measure_batch(process_func, batch, extract_event_time_ms):
         "records_processed": len(batch),
         "avg_latency_ms": sum(latencies) / len(latencies),
         "max_latency_ms": max(latencies),
-        "p95_latency": sorted(latencies)[int(0.95 * (len(latencies) - 1))],
+        "p95_latency_ms": sorted(latencies)[int(0.95 * (len(latencies) - 1))],
     }
     return result, metrics
